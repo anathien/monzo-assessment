994cbef56a76d32b2eafe132a109c2b319e55d2b
Adrienn Eva Csengeri-Pap <eva@zzish.com>
2018-09-18 21:23:06 +0100

    Basic pager control

--- src/client/views/App.js
+++ src/client/views/App.js
@@ -5,6 +5,9 @@ import idx from "idx";
 import { getApps, getUsers } from "./../utils/appUtils";
 import AuthStateStore, { KEYS } from "./../stores/AuthStateStore";
 
+import backIcon from "../assets/chevron-left-solid.svg";
+import nextIcon from "../assets/chevron-right-solid.svg";
+
 import styles from "./AppStyles.css";
 
 type Props = {
@@ -13,13 +16,17 @@ type Props = {
 type State = {
     app: Object,
     users: Array<Object>,
+    userOffset: number,
 };
+
+const PAGE_SIZE = 25;
 
 export class App extends React.Component<Props, State> {
     props: Props;
     state: State = {
         app: {},
         users: [],
+        userOffset: 0,
     };
 
     componentDidMount() {
@@ -44,20 +51,37 @@ export class App extends React.Component<Props, State> {
     refreshApp = () => {
         // TODO: validate on appId being present
         // Note: I am very truly sorry. I can't even begin to tell you how suboptimal this is...
-        Promise.all([getApps(), getUsers(this.props.appId)]).then(promiseResults => {
+        Promise.all([getApps(), getUsers(this.props.appId, 0)]).then(promiseResults => {
             console.log("promiseResults", promiseResults);
             this.setState({
                 app: promiseResults[0].find(app => app.id === this.props.appId) || {},
                 users: promiseResults[1],
+                userOffset: 0,
             });
         });
     };
 
     render() {
+        // Note: this could be moved to a component
+        const pagingControl = (
+            <div className={styles.pagingContainer}>
+                <button className={styles.pagingButton}>
+                    <img src={backIcon} alt="Previous page" />
+                </button>
+                <div className={styles.pagingIndicator}>Page 0</div>
+                <button className={styles.pagingButton}>
+                    <img src={nextIcon} alt="Next page" />
+                </button>
+            </div>
+        );
+
         return (
             <Fragment>
                 <h1>{idx(this.state, _ => _.app.name)}</h1>
-                <div className={styles.userTitle}>Users:</div>
+                <div className={styles.gridHeader}>
+                    <div className={styles.userTitle}>Users:</div>
+                    {pagingControl}
+                </div>
                 <div className={styles.gridList}>
                     {this.state.users.map(user => {
                         return (
@@ -73,6 +97,7 @@ export class App extends React.Component<Props, State> {
                         );
                     })}
                 </div>
+                {pagingControl}
             </Fragment>
         );
     }
